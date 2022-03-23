3198a789674dae7e58c5380904cd3899f676ac38
Youngnam <lyn00227@naver.com>
2022-03-23 14:57:13 +0900

    web config

---
+++ src/main/java/api/personalitytest/WebConfig.java
@@ -0,0 +1,33 @@
+package api.personalitytest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@PropertySource("classpath:application-dir.properties")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @Value("${client.url}")
+    private String clientURL;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(clientURL)
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:///" + fileDir);
+    }
+}
